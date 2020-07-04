c2e5557f6fe00cb398394e0df010a14231e79e4f
namortegui <62971954+namortegui@users.noreply.github.com>
2020-07-04 17:33:10 -0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -3,8 +3,3 @@ var speakWord = "Hello";
 helloSpeaker.speak = function speak(name) {
   console.log(speakWord + " " + name);
 }
-var byeSpeaker={};
-var speakWord2 = "Good Bye";
-byeSpeaker.speak = function speak(name) {
-  console.log(speakWord2 + " " + name);
-}
